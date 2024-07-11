2329b8590b152881f7379cf6727eae41b2b02c74
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-07-11 11:58:25 +0200

    add(set up Post.php )

---
+++ model/entities/Post.php
@@ -0,0 +1,102 @@
+<?php
+namespace Model\Entities;
+
+use App\Entity;
+
+/*
+    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
+*/
+
+final class Category extends Entity{
+
+    private $id;
+
+    private $message;
+
+    private $topic;
+
+    private $user;
+    
+    private $creationDate;
+
+    public function __construct($data){         
+        $this->hydrate($data);        
+    }
+    
+    /**
+     * Get the value of creationDate
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set the value of creationDate
+     */
+    public function setCreationDate($creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     */
+    public function setUser($user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of topic
+     */
+    public function getTopic()
+    {
+        return $this->topic;
+    }
+
+    /**
+     * Set the value of topic
+     */
+    public function setTopic($topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->message;
+    }
+}
